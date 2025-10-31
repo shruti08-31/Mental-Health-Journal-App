5956769199bad7e7d5616e7370870f982279c03d
SHRUTI <shrutiprasad0827@gmail.com>
2025-10-31 15:15:26 +0530

    Update TagManager.java

--- src/services/TagManager.java
+++ src/services/TagManager.java
@@ -1,1 +1,19 @@
+package service;
+
+import java.time.LocalDate;
+import java.util.*;
+
+public class TagManager {
+    private Map<LocalDate, Set<String>> tags = new HashMap<>();
+
+    public void addTag(LocalDate date, String tag) {
+        // Always store tags in lowercase for consistency
+        String lowerTag = tag.toLowerCase();
+        if (!tags.containsKey(date)) {
+            tags.put(date, new HashSet<String>());
+        }
+        tags.get(date).add(lowerTag);
+    }
+
+}
 
