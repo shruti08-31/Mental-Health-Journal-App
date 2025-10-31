c435ea709353f98eac5de8406e45cb6313381796
SHRUTI <shrutiprasad0827@gmail.com>
2025-10-31 14:57:10 +0530

    Update JournalManager.java

--- src/services/JournalManager.java
+++ src/services/JournalManager.java
@@ -1,1 +1,22 @@
+package service;
+
+import model.JournalEntry;
+import java.time.LocalDate;
+import java.util.*;
+
+public class JournalManager {
+    private List<JournalEntry> entries = new ArrayList<>();
+
+    public void addEntry(JournalEntry e) {
+        entries.add(e);
+    }
+
+    public boolean deleteEntry(int id) {
+        return entries.removeIf(e -> e.getId() == id);
+    }
+
+    public List<JournalEntry> getAllEntries() {
+        return new ArrayList<>(entries);
+    }
+}
 
