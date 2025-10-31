4299838c34d59e613c621f397fd2683c39b2f36c
SHRUTI <shrutiprasad0827@gmail.com>
2025-10-31 15:33:08 +0530

    Create Goal class with basic attributes and methods

---
+++ src/model/Goal.java
@@ -0,0 +1,39 @@
+package model;
+
+public class Goal {
+    private int id;
+    private String title;
+    private String description;
+    private boolean completed;
+    public Goal(int id, String title, String description) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.completed = false;
+    }
+    public int getId() { 
+      
+      return id; 
+      
+    }
+    public String getTitle() { 
+      
+      return title; 
+      
+    }
+    public String getDescription() { 
+      
+      return description; 
+      
+    }
+    public boolean isCompleted() { 
+      
+      return completed; 
+    }
+
+    public void markCompleted() {
+        this.completed = true;
+    }
+
+    //ToString method likhna hai abhi
+}
