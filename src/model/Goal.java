1ee64e0ff1234b03346813a3c00e1622039c7689
SHRUTI <shrutiprasad0827@gmail.com>
2025-11-06 15:15:26 +0530

    Refactor Goal class getters and toString method

--- src/model/Goal.java
+++ src/model/Goal.java
@@ -1,39 +1,38 @@
 package model;
 
+
 public class Goal {
     private int id;
     private String title;
     private String description;
     private boolean completed;
+
     public Goal(int id, String title, String description) {
         this.id = id;
         this.title = title;
         this.description = description;
         this.completed = false;
     }
+
     public int getId() { 
-      
-      return id; 
-      
-    }
+        return id; 
+        }
     public String getTitle() { 
-      
-      return title; 
-      
-    }
-    public String getDescription() { 
-      
-      return description; 
-      
-    }
+        return title;
+        }
+    public String getDescription() {
+        return description;
+        }
     public boolean isCompleted() { 
-      
-      return completed; 
-    }
+        return completed; 
+        }
 
-    public void markCompleted() {
-        this.completed = true;
-    }
+    public void markCompleted() { 
+        this.completed = true; 
+        }
 
-    //ToString method likhna hai abhi
+    @Override
+    public String toString() {
+        return "Goal{id=" + id + ", title='" + title + "', completed=" + completed + "}";
+    }
 }
