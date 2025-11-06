02d33dec4968d6492860a7b502aad73650f1def3
SHRUTI <shrutiprasad0827@gmail.com>
2025-11-06 15:14:13 +0530

    Refactor JournalEntry class structure and methods
    Refactored JournalEntry class by removing id and tags, updating constructor and methods.

--- src/model/JournalEntry.java
+++ src/model/JournalEntry.java
@@ -5,82 +5,36 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class JournalEntry {
-    private int id;
+    private String title;     // Tag (mood + date)
+    private String content;   // Reflection answer
     private LocalDate date;
-    private String title;
-    private String content;
     private String mood;
-    private List<String> tags;
-    public JournalEntry(int id, LocalDate date, String title, String content, String mood) {
-        this.id = id;
-        this.date = date;
+
+    public JournalEntry(String title, String content, String mood, LocalDate date) {
         this.title = title;
         this.content = content;
         this.mood = mood;
-        this.tags = new ArrayList<>();
-    }
-    public int getId() { 
-      return id; 
-    }
-    public void setId(int id) { 
-      
-      this.id = id; 
-      
+        this.date = date;
     }
 
-    public LocalDate getDate() { 
-      
-      return date; 
-      
-    }
-    public void setDate(LocalDate date) { 
-      
-      this.date = date; 
-      
+    public String getTitle() {
+        return title;
     }
 
-    public String getTitle() { 
-      
-      return title; 
-      
-    }
-    public void setTitle(String title) { 
-      
-      this.title = title; 
-      
+    public String getContent() {
+        return content;
     }
 
-    public String getContent() { 
-      
-      return content; 
-      
-    }
-    public void setContent(String content) { 
-      
-      this.content = content; 
-      
+    public LocalDate getDate() {
+        return date;
     }
 
-    public String getMood() { 
-      return mood; 
-    }
-    public void setMood(String mood) { 
-      this.mood = mood; 
-    }
-
-    public List<String> getTags() { 
-      
-      return tags; 
-    }
-
-    public void addTag(String tag) {
-      if (!tags.contains(tag)) {
-        tags.add(tag);
-      }
+    public String getMood() {
+        return mood;
     }
 
     @Override
     public String toString() {
-        return "entry [ " + date + " ] " + title + " | mood: " + mood + "\n" + content;
+        return "Entry{" + " title='" + title + '\'' + ", mood='" + mood + '\'' + ", date=" + date + ", content='" + content + '\'' + '}';
     }
 }
