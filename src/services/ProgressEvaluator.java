3b752319906434a13934bf7d491b3b3b4c9efe9d
PratimaChauhan2606 <pratimachauhan2606@gmail.com>
2025-11-09 21:55:24 +0530

    Update ProgressEvaluator.java

--- src/services/ProgressEvaluator.java
+++ src/services/ProgressEvaluator.java
@@ -6,7 +6,7 @@ public class ProgressEvaluator {
 
     public String checkProgress(User user) {
         if (user.getStreakCount() >= 7)
-            return "🌟 You’ve completed a 7-day mood streak!";
+            return " You’ve completed a 7-day mood streak!";
         else if (user.getStreakCount() >= 3)
             return "🌼 You’re building great consistency!";
         return "Keep writing daily — every word helps.";
