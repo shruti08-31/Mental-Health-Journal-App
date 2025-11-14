385efd33e523dcd0e02eecb4ee4165389c7c6104
SHRUTI <shrutiprasad0827@gmail.com>
2025-11-14 13:24:34 +0530

    Update Tag.java

--- src/model/Tag.java
+++ src/model/Tag.java
@@ -1,26 +1,46 @@
 package model;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class Tag {
-    private String name;
+    private String mood;
     private LocalDate date;
+    private LocalTime time;
+    private String tagText;
 
-    public Tag(String name, LocalDate date) {
-        this.name = name.toLowerCase();
-        this.date = date;
+    public Tag(String mood) {
+        this.mood = mood;
+        this.date = LocalDate.now();
+        this.time = LocalTime.now();
+        this.tagText = generateTagText();
     }
 
-    public String getName() {
-        return name;
+    private String generateTagText() {
+        return mood.toLowerCase() + "-" + date + "-" + time.toString().substring(0,5);
     }
 
+    public String getMood() {
+        
+        return mood;
+    }
     public LocalDate getDate() {
+        
         return date;
+    }
+    public LocalTime getTime() {
+        
+        return time;
+    }
+    public String getTagText() {
+        
+        return tagText;
     }
 
     @Override
     public String toString() {
-        return name + " ( " + date + " ) ";
+        
+        return tagText;
     }
 }
+
