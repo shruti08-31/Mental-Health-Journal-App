bbba10fe84caba8657bd8f66b55e238f2fa61ed5
SHRUTI <shrutiprasad0827@gmail.com>
2025-11-14 13:23:54 +0530

    Update JournalEntry.java

--- src/model/JournalEntry.java
+++ src/model/JournalEntry.java
@@ -1,43 +1,38 @@
 package model;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.UUID;
-
 public class JournalEntry {
-    private String id;
-    private String title;
     private String mood;
     private String content;
-    private LocalDateTime dateTime;
+    private String intensity; // NEW FIELD
+    private Tag tag;
 
-    public JournalEntry(String mood, String content) {
-        this.id = UUID.randomUUID().toString();//isko khud se dal rhi hai isko mt dal hm isko user ke id se link kr denge so inko int hi rehne de
+    public JournalEntry(String mood, String content, String intensity) {
         this.mood = mood;
         this.content = content;
-        this.dateTime = LocalDateTime.now();
-        this.title = mood + "_" + dateTime.toString();
+        this.intensity = intensity;
     }
 
-    public String getMood() { 
-        return mood; 
+    public String getMood() {
+        return mood;
+    }
+    public String getContent() {
+        return content;
+    }
+    public String getIntensity() {
+        return intensity;
     }
 
-    public String getContent() { 
-        return content; 
+    public void setTag(Tag tag) {
+        this.tag = tag;
     }
 
-    public String getTitle() { 
-        return title; 
+    public Tag getTag() {
+        return tag;
     }
 
-    public LocalDateTime getDateTime() { 
-        return dateTime; 
-    }
-
-   
-    public LocalDate getDate() {
-        return dateTime.toLocalDate();
+    @Override
+    public String toString() {
+        return "Mood: " + mood + "\nIntensity: " + intensity + "\nTag: " + tag.getTagText() + "\nContent: " + content + "\n";
     }
 }
 
