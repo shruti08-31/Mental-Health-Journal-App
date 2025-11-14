7adc72f7e4caff75f71e37d9be3435e09c355a12
SHRUTI <shrutiprasad0827@gmail.com>
2025-11-14 13:25:39 +0530

    Update TagManager.java

--- src/services/TagManager.java
+++ src/services/TagManager.java
@@ -1,9 +1,31 @@
 package services;
 
-import java.time.LocalDateTime;
+import model.Tag;
+import java.util.*;
 
 public class TagManager {
-    public String makeTag(String mood) {
-        return mood + "_" + LocalDateTime.now();
+    private List<Tag> tags = new ArrayList<>();
+
+    //create and store a new tag
+    public Tag createTag(String mood) {
+        Tag tag = new Tag(mood);
+        tags.add(tag);
+        return tag;
+    }
+
+    //retrieve all tags
+    public List<Tag> getAllTags() {
+        return tags;
+    }
+
+    //search tags by mood
+    public List<Tag> getTagsByMood(String mood) {
+        List<Tag> result = new ArrayList<>();
+        for (Tag t : tags) {
+            if (t.getMood().equalsIgnoreCase(mood)) {
+                result.add(t);
+            }
+        }
+        return result;
     }
 }
