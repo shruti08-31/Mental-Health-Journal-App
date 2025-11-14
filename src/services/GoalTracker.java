c74c07564d9afd4b3f63b30ab9e2e1d7d433c0b5
PratimaChauhan2606 <pratimachauhan2606@gmail.com>
2025-11-14 13:24:20 +0530

    Update GoalTracker.java

--- src/services/GoalTracker.java
+++ src/services/GoalTracker.java
@@ -1,15 +1,56 @@
+// Source code is decompiled from a .class file using FernFlower decompiler (from Intellij IDEA).
 package services;
 
-import model.User;
+import java.io.PrintStream;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
+import model.Goal;
 
 public class GoalTracker {
-    private int level = 1;
+   private GoalRecommender recommender = new GoalRecommender();
+   private Scanner sc;
 
-    public String levelUp(User user) {
-        if (user.getMoodScore() > 80 && user.getStreakCount() > 5) {
-            level++;
-            return " You reached Level " + level + "! Keep the positive energy!";
-        }
-        return "Stay consistent — you’re getting closer to the next level!";
-    }
+   public GoalTracker() {
+      this.sc = new Scanner(System.in);
+   }
+
+   public void trackGoals(String mood) {
+      List<Goal> goals = this.recommender.recommendGoals(mood);
+      System.out.println("\nBased on your mood, here are your goals:");
+      Iterator var3 = goals.iterator();
+
+      PrintStream var10000;
+      String var10001;
+      Goal g;
+      while(var3.hasNext()) {
+         g = (Goal)var3.next();
+         var10000 = System.out;
+         var10001 = g.getTitle();
+         var10000.println("- " + var10001 + ": " + g.getDescription());
+      }
+
+      System.out.println("\nHave you completed these goals? (yes/no)");
+      var3 = goals.iterator();
+
+      while(var3.hasNext()) {
+         g = (Goal)var3.next();
+         System.out.print(g.getTitle() + " → ");
+         String ans = this.sc.nextLine().trim().toLowerCase();
+         if (ans.equals("yes")) {
+            g.markCompleted();
+         }
+      }
+
+      System.out.println("\nGoal Progress:");
+      var3 = goals.iterator();
+
+      while(var3.hasNext()) {
+         g = (Goal)var3.next();
+         var10000 = System.out;
+         var10001 = g.getTitle();
+         var10000.println(var10001 + " - " + (g.isCompleted() ? "Completed" : "Pending"));
+      }
+
+   }
 }
